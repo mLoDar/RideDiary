d6a95c9c1bf10197a40cff9ba151e09d2f7068bc
mLoDar <tomas.wampenwachser@gmail.com>
2024-01-03 22:15:31 +0100

    Added resources The new file will include values which needs to be used application wide

---
+++ Resources/ApplicationValues.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+
+
+
+
+namespace RideDiary.Resources
+{
+    internal class ApplicationValues
+    {
+        internal static readonly string path_AppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        internal static readonly string path_SaveFile = Path.Combine(path_AppData, "RideDiaryData.json");
+    }
+}

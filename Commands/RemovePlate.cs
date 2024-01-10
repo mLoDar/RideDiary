6cce791b46509c70537ecb5d8c5bd50cafbdbc1e
mLoDar <tomas.wampenwachser@gmail.com>
2024-01-11 00:20:31 +0100

    Implemented plate removing

--- Commands/RemovePlate.cs
+++ Commands/RemovePlate.cs
@@ -1,4 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using RideDiary.Scripts;
+using RideDiary.Resources;
+
+using Newtonsoft.Json.Linq;
 
 
 
@@ -8,9 +16,232 @@ namespace RideDiary.Commands
 {
     internal class RemovePlate
     {
+        private static JObject rideDiaryData = new();
+
+
+
+
+
         internal static async Task Start()
         {
+        MethodBeginning:
 
+            Console.Title = "RideDiary | Remove added number plate";
+            DisplayUI.ResetConsole();
+
+            LoadDataFromFile();
+
+            if (rideDiaryData.ContainsKey("error"))
+            {
+                await DisplayUI.DisplayError($"{rideDiaryData["error"]}");
+                return;
+            }
+
+            if (rideDiaryData.ContainsKey("plates") == false)
+            {
+                rideDiaryData["plates"] = new JArray();
+            }
+
+
+
+            DisplayUI.ResetConsole();
+
+            JArray plates = rideDiaryData["plates"] as JArray ?? new();
+
+            if (plates.Count <= 0)
+            {
+                await DisplayUI.DisplayError("                 No number plates have been added yet");
+                return;
+            }
+
+
+
+            for (int i = 0; i < plates.Count; i++)
+            {
+                JProperty? currentPlateProperty = (plates.ElementAt(i) as JObject ?? new JObject()).Properties().FirstOrDefault();
+                string numberPlate = currentPlateProperty?.Name ?? string.Empty;
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"                 [{i + 1}] ");
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"{numberPlate}");
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"                 ");
+            Console.WriteLine($"                 Enter the number of the plate to remove");
+
+
+
+        LabelNumberInput:
+
+            Console.Write($"                 > ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string enteredNumber = Console.ReadLine() ?? string.Empty;
+
+            if (ValidNumberPlateSelection(enteredNumber, plates) == false)
+            {
+                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+
+                DisplayUI.ClearLine();
+
+                goto LabelNumberInput;
+            }
+
+
+
+            int plateIndex = Convert.ToInt32(enteredNumber) - 1;
+            JObject plateToRemove = plates.ElementAtOrDefault(plateIndex) as JObject ?? new JObject();
+            JProperty? plateProperty = plateToRemove.Properties().FirstOrDefault();
+
+            if (plateProperty == null)
+            {
+                await DisplayUI.DisplayError($"                 An unexpected error appeared, please try again");
+                return;
+            }
+
+
+
+            DisplayUI.ResetConsole();
+
+
+            
+            string plate_Name = plateProperty.Name;
+            string plate_Maker = $"{plateToRemove?[plate_Name]?["Car_Maker"]}";
+            string plate_Model = $"{plateToRemove?[plate_Name]?["Car_Model"]}";
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"                 Number plate: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"'{plate_Name}'");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"                 Car maker: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"'{plate_Maker}'");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"                 Car model: ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"'{plate_Model}'");
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"                 ");
+            Console.Write($"                 Are you sure you want to ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"REMOVE");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($" the selected plate?");
+
+            Console.WriteLine($"                 ");
+            Console.Write($"                 > (y/n): ");
+
+
+
+        LabelKeyRead:
+
+            char pressedKey = Console.ReadKey(true).KeyChar;
+
+            switch (pressedKey)
+            {
+                case 'y':
+                    break;
+
+                case 'n':
+                    goto MethodBeginning;
+
+                case (char)ConsoleKey.Escape:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    goto LabelKeyRead;
+            }
+
+
+
+            plates.ElementAtOrDefault(plateIndex)?.Remove();
+            rideDiaryData["plates"] = plates;
+
+
+
+            JObject result = await SaveFileHandler.SaveDataToFile(rideDiaryData);
+
+            if (result.ContainsKey("error"))
+            {
+                await DisplayUI.DisplayError($"                 {result["error"]}");
+                return;
+            }
+
+
+
+            DisplayUI.ResetConsole();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("                 Successfully removed the selected plate");
+            await Task.Delay(3000);
+        }
+
+        private static void LoadDataFromFile()
+        {
+            DisplayUI.ResetConsole();
+
+            Console.Write("                 Loading save file");
+
+
+
+            bool saveFileLoaded = false;
+
+
+
+            Task task_SaveFileLoading = new(async () =>
+            {
+                rideDiaryData = await SaveFileHandler.LoadDataFromFile();
+
+                saveFileLoaded = true;
+            });
+
+            Task task_LoadingAnimation = new(async () =>
+            {
+                while (saveFileLoaded == false)
+                {
+                    Console.Write(" .");
+                    await Task.Delay(1000);
+                }
+            });
+
+
+
+            task_SaveFileLoading.Start();
+            task_LoadingAnimation.Start();
+
+            while (saveFileLoaded == false)
+            {
+
+            }
+        }
+
+        private static bool ValidNumberPlateSelection(string enteredNumber, JArray plates)
+        {
+            if (RegexPatterns.AllWhitespaces().Replace(enteredNumber, string.Empty).Equals(string.Empty))
+            {
+                return false;
+            }
+
+            if (int.TryParse(enteredNumber, out int convertedNumber) == false)
+            {
+                return false;
+            }
+
+            if (Enumerable.Range(1, plates.Count).Contains(convertedNumber) == false)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }

7fe22b9dedd25e217b63d38e027c705cdf094489
mLoDar <tomas.wampenwachser@gmail.com>
2024-01-11 11:09:22 +0100

    Implemented data adding Things such as expenses and trips can now be added to a selected plate

--- Commands/AddDataToPlate.cs
+++ Commands/AddDataToPlate.cs
@@ -1,4 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Globalization;
+using System.Threading.Tasks;
+
+using RideDiary.Scripts;
+using RideDiary.Resources;
+
+using Newtonsoft.Json.Linq;
 
 
 
@@ -8,9 +16,418 @@ namespace RideDiary.Commands
 {
     internal class AddDataToPlate
     {
+        private static JObject rideDiaryData = new();
+
+
+
+
+
         internal static async Task Start()
         {
+            Console.Title = "RideDiary | Add data to plate";
+            DisplayUI.ResetConsole();
 
+            LoadDataFromFile();
+
+            if (rideDiaryData.ContainsKey("error"))
+            {
+                await DisplayUI.DisplayError($"                 {rideDiaryData["error"]}");
+                return;
+            }
+
+            if (rideDiaryData.ContainsKey("plates") == false)
+            {
+                rideDiaryData["plates"] = new JArray();
+            }
+
+
+
+            DisplayUI.ResetConsole();
+
+
+
+            JArray plates = rideDiaryData["plates"] as JArray ?? new();
+
+            if (plates.Count <= 0)
+            {
+                await DisplayUI.DisplayError("                 No number plates have been added yet");
+                return;
+            }
+
+
+            DisplayNumberPlates(plates);
+
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"                 ");
+            Console.WriteLine($"                 Enter the number of the plate to add data to");
+
+
+
+        LabelNumberInput:
+
+            Console.Write($"                 > ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string enteredNumber = Console.ReadLine() ?? string.Empty;
+
+            if (ValidNumberPlateSelection(enteredNumber, plates) == false)
+            {
+                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+
+                DisplayUI.ClearLine();
+
+                goto LabelNumberInput;
+            }
+
+
+
+            int plateIndex = Convert.ToInt32(enteredNumber) - 1;
+            JObject selectedPlate = plates.ElementAtOrDefault(plateIndex) as JObject ?? new JObject();
+            JProperty? plateProperty = selectedPlate.Properties().FirstOrDefault();
+
+            if (plateProperty == null)
+            {
+                await DisplayUI.DisplayError($"                 An unexpected error appeared, please try again");
+                return;
+            }
+
+
+
+            DisplayUI.ResetConsole();
+
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 Which data do you want to add?");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("                 ______________________________");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 ");
+            Console.WriteLine("                 [1] New trip");
+            Console.WriteLine("                 [2] New expenses");
+            Console.WriteLine("                 ");
+            Console.Write("                 > ");
+
+
+        LabelKeyRead:
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            char pressedKey = Console.ReadKey(true).KeyChar;
+
+            switch (pressedKey)
+            {
+                case '1':
+                    selectedPlate = AddDataNewTrip(selectedPlate, plateProperty.Name);
+                    break;
+
+                case '2':
+                    selectedPlate = AddDataNewExpenses(selectedPlate, plateProperty.Name);
+                    break;
+
+                case (char)ConsoleKey.Escape:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    goto LabelKeyRead;
+            }
+
+
+
+            plates[plateIndex] = selectedPlate;
+            rideDiaryData["plates"] = plates;
+
+
+
+            JObject result = await SaveFileHandler.SaveDataToFile(rideDiaryData);
+
+            if (result.ContainsKey("error"))
+            {
+                await DisplayUI.DisplayError($"                 {result["error"]}");
+                return;
+            }
+
+
+
+            DisplayUI.ResetConsole();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("                 Successfully added data to the selected plate");
+            await Task.Delay(3000);
+        }
+
+        private static void LoadDataFromFile()
+        {
+            DisplayUI.ResetConsole();
+
+            Console.Write("                 Loading save file");
+
+
+
+            bool saveFileLoaded = false;
+
+
+
+            Task task_SaveFileLoading = new(async () =>
+            {
+                rideDiaryData = await SaveFileHandler.LoadDataFromFile();
+
+                saveFileLoaded = true;
+            });
+
+            Task task_LoadingAnimation = new(async () =>
+            {
+                while (saveFileLoaded == false)
+                {
+                    Console.Write(" .");
+                    await Task.Delay(1000);
+                }
+            });
+
+
+
+            task_SaveFileLoading.Start();
+            task_LoadingAnimation.Start();
+
+            while (saveFileLoaded == false)
+            {
+
+            }
+        }
+
+        private static void DisplayNumberPlates(JArray plates)
+        {
+            for (int i = 0; i < plates.Count; i++)
+            {
+                JProperty? currentPlateProperty = (plates.ElementAt(i) as JObject ?? new JObject()).Properties().FirstOrDefault();
+                string numberPlate = currentPlateProperty?.Name ?? string.Empty;
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"                 [{i + 1}] ");
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"{numberPlate}");
+            }
+        }
+
+        private static bool ValidNumberPlateSelection(string enteredNumber, JArray plates)
+        {
+            if (RegexPatterns.AllWhitespaces().Replace(enteredNumber, string.Empty).Equals(string.Empty))
+            {
+                return false;
+            }
+
+            if (int.TryParse(enteredNumber, out int convertedNumber) == false)
+            {
+                return false;
+            }
+
+            if (Enumerable.Range(1, plates.Count).Contains(convertedNumber) == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static JObject AddDataNewTrip(JObject plateToAddDataTo, string numberPlate)
+        {
+            Console.Title = "RideDiary | Add new trip to plate";
+            DisplayUI.ResetConsole();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 ENTER INFORMATION");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("                 _________________");
+            Console.WriteLine();
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 Enter the kilometer reading at the beginning of the trip");
+        LabelReadKilometersStart:
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("                 > ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string kilometers_Start = Console.ReadLine() ?? string.Empty;
+
+            if (int.TryParse(kilometers_Start, out _) == false)
+            {
+                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+
+                DisplayUI.ClearLine();
+
+                goto LabelReadKilometersStart;
+            }
+
+            
+            
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 Enter the kilometer reading at the end of the trip");
+        LabelReadKilometersEnd:
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("                 > ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string kilometers_End = Console.ReadLine() ?? string.Empty;
+
+            if (int.TryParse(kilometers_End, out int result) == false || result < Convert.ToInt32(kilometers_Start))
+            {
+                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+
+                DisplayUI.ClearLine();
+
+                goto LabelReadKilometersEnd;
+            }
+
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 Enter the date of the trip (format: day.month.year)");
+        LabelReadDate:
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("                 > ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string trip_Date = Console.ReadLine() ?? string.Empty;
+
+            if (DateTime.TryParseExact(trip_Date, "dd.MM.yyyy", null, DateTimeStyles.None, out _) == false)
+            {
+                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+
+                DisplayUI.ClearLine();
+
+                goto LabelReadDate;
+            }
+
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 Enter description for the trip");
+        LabelReadDescription:
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("                 > ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string trip_Description = Console.ReadLine() ?? string.Empty;
+
+            if (RegexPatterns.AllWhitespaces().Replace(trip_Description, string.Empty).Equals(string.Empty))
+            {
+                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+
+                DisplayUI.ClearLine();
+
+                goto LabelReadDescription;
+            }
+
+
+
+            JArray kilometerDiary = plateToAddDataTo[numberPlate]["Car_KilometerDiary"] as JArray ?? new JArray();
+
+            kilometerDiary.Add(
+                new JObject()
+                {
+                    ["Kilometers_Start"] = kilometers_Start,
+                    ["Kilometers_End"] = kilometers_End,
+                    ["Trip_Date"] = trip_Date,
+                    ["Trip_Description"] = trip_Description
+                }
+            );
+
+            plateToAddDataTo[numberPlate]["Car_KilometerDiary"] = kilometerDiary;
+            return plateToAddDataTo;
+        }
+
+        private static JObject AddDataNewExpenses(JObject plateToAddDataTo, string numberPlate)
+        {
+            Console.Title = "RideDiary | Add new expenses to plate";
+            DisplayUI.ResetConsole();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 ENTER INFORMATION");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("                 _________________");
+            Console.WriteLine();
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 Enter the amount of the expenses in Euro");
+        LabelReadKilometersStart:
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("                 > ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string expenses_Amount = Console.ReadLine() ?? string.Empty;
+            expenses_Amount.Replace("€", string.Empty);
+
+            if (decimal.TryParse(expenses_Amount, out _) == false)
+            {
+                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+
+                DisplayUI.ClearLine();
+
+                goto LabelReadKilometersStart;
+            }
+
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 Enter the date of the expenses");
+        LabelReadDate:
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("                 > ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string expenses_Date = Console.ReadLine() ?? string.Empty;
+
+            if (DateTime.TryParseExact(expenses_Date, "dd.MM.yyyy", null, DateTimeStyles.None, out _) == false)
+            {
+                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+
+                DisplayUI.ClearLine();
+
+                goto LabelReadDate;
+            }
+
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 Enter a description for the expenses");
+        LabelReadDescription:
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("                 > ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string expenses_Description = Console.ReadLine() ?? string.Empty;
+
+            if (RegexPatterns.AllWhitespaces().Replace(expenses_Description, string.Empty).Equals(string.Empty))
+            {
+                Console.SetCursorPosition(0, Console.GetCursorPosition().Top - 1);
+
+                DisplayUI.ClearLine();
+
+                goto LabelReadDescription;
+            }
+
+
+
+            JArray expensesDiary = plateToAddDataTo[numberPlate]?["Car_ExpensesDiary"] as JArray ?? new JArray();
+
+            expensesDiary.Add(
+                new JObject()
+                {
+                    ["Date"] = expenses_Date,
+                    ["AmountEur"] = expenses_Amount,
+                    ["Description"] = expenses_Description
+                }
+            );
+
+            plateToAddDataTo[numberPlate]["Car_ExpensesDiary"] = expensesDiary;
+            return plateToAddDataTo;
         }
     }
 }

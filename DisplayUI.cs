fc24545af116f4b53164bdaa6d3cb65a6e51ce3b
mLoDar <mlodarcoding@gmail.com>
2024-03-22 21:56:52 +0100

    Added root menu for further selection

---
+++ DisplayUI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+
+
+
+namespace RideDiary
+{
+    internal class DisplayUI
+    {
+        internal static void RootMenu()
+        {
+            ResetUI();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 RideDiary");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("                 __________________");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 ");
+            Console.WriteLine("                 [1] Add new number plate");
+            Console.WriteLine("                 [2] Add data to an existing number plate");
+            Console.WriteLine("                 [3] View data for a number plate");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("                 __________________");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                 ");
+            Console.WriteLine("                 [4] Statistics");
+        }
+
+        internal static void ResetUI()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, 5);
+        }
+    }
+}
--- MainEntry.cs
+++ MainEntry.cs
@@ -1,17 +1,10 @@
-﻿using System;
-
-
-
-
-
-namespace RideDiary
+﻿namespace RideDiary
 {
     internal class MainEntry
     {
         internal static void Main()
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadKey();
+            DisplayUI.RootMenu();
         }
     }
 }

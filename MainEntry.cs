ec9104226dc5925f6158fc9deaee6268ffb48e63
mLoDar <mlodarcoding@gmail.com>
2024-03-22 21:56:51 +0100

    Add project files.

---
+++ MainEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+
+
+
+namespace RideDiary
+{
+    internal class MainEntry
+    {
+        internal static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}

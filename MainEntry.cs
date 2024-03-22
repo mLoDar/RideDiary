c5ea65ee11608512b9af8d995778bf64434e2645
mLoDar <mlodarcoding@gmail.com>
2024-03-22 21:56:52 +0100

    Added key handling for the root menu when starting the application

--- MainEntry.cs
+++ MainEntry.cs
@@ -1,10 +1,50 @@
-﻿namespace RideDiary
+﻿using System;
+
+
+
+
+
+namespace RideDiary
 {
     internal class MainEntry
     {
         internal static void Main()
         {
+        LabelMethodBeginning:
+
+            Console.Title = "RideDiary | Root";
             DisplayUI.RootMenu();
+
+
+
+        LabelKeyRead:
+            char pressedKey = Console.ReadKey(true).KeyChar;
+
+            switch (pressedKey)
+            {
+                case '1':
+                    break;
+
+                case '2':
+                    break;
+
+                case '3':
+                    break;
+
+                case '4':
+                    break;
+
+                case (char)ConsoleKey.Escape:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    goto LabelKeyRead;
+            }
+
+
+
+            goto LabelMethodBeginning;
         }
     }
 }
